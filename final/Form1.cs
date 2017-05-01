39a70469dc4eea06e48b4fe01c5fabe713c8cefc
Samuel <s950173@gmail.com>
2017-05-01 12:02:51 +0800

    first add

---
+++ final/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("button1 show");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1 .Text =textBox1 .Text ;
+        }
+    }
+}
